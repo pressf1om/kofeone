9693205c2bb4b4b21c93ec38961ec1a0d105de60
pressf1om <wwwsasha.www@yandex.ru>
2024-09-18 13:20:16 +0300

    final commit

--- cofeeone/cofeeone/settings.py
+++ cofeeone/cofeeone/settings.py
@@ -122,12 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'recept/static'),
-    os.path.join(BASE_DIR, 'main/static'),
-    os.path.join(BASE_DIR, 'about/static'),
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
