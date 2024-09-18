3704da4692d054b68ce869e3268574fb94332977
Woonze <kirillatur1@gmail.com>
2024-09-18 11:38:43 +0300

    Предрелиз: Добавил футор, бд для формы обратной связи, ссылка на кнопку на главной, endpoint хоть и пустоц. Не получилось: Синхронизировать бд и форму обратной связи, static/css для приложений, страница оплаты не обновлена

--- cofeeone/main/models.py
+++ cofeeone/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Feedback(models.Model):
+    email = models.CharField(max_length=100, verbose_name="Почта")
+    text = models.TextField(verbose_name="Сообщение")
+
+    def __str__(self):
+        return self.name
--- cofeeone/main/views.py
+++ cofeeone/main/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from .forms import ContactForm
 
 def main(request):
     return render(request, 'main/main.html')
+
+"""def feedback(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return
+        redirect('thanks')
+    else:
+        form = ContactForm() """
